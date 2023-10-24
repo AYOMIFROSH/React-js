d279b65dbf3f6fe28dea0cf3fc65474409a8b5ca
tayo <rhymeanabel@gmail.com>
2023-10-24 12:19:34 +0100

    adding ReactLogo.jpg

---
+++ src/switch.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const main = () => {
+    <React.Fragment>
+        <header>
+            <img className="logo" src="" alt="logo" />
+            <nav>
+                <ul class="navbar">
+                    <li><a href="/Page.html">About Us</a> </li>
+                    <li><a href="#">Career</a> </li>
+                    <li><a href="#">Departments</a> </li>
+                </ul>
+            </nav>
+        </header>
+    </React.Fragment>
+}
