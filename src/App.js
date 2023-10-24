b4dd56097ae3e84a5514d4fc167a6c2f346987cf
tayo <rhymeanabel@gmail.com>
2023-10-24 19:29:55 +0100

    updating package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import Main from './switch';
+import './App.css';
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+
+function App() {
+    return (
+    <div className="App">
+        <Main />
+    </div>
+    );
+}
+
+export default App;
 
--- src/switch.jsx
+++ src/switch.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import Sign from "./sign";
+import { Link } from 'react-router-dom';
 
-const main = () => {
+const Main = () => {
+    return (
     <React.Fragment>
-        <header>
-            <img className="logo" src="" alt="logo" />
-            <nav>
-                <ul class="navbar">
-                    <li><a href="/Page.html">About Us</a> </li>
-                    <li><a href="#">Career</a> </li>
-                    <li><a href="#">Departments</a> </li>
-                </ul>
-            </nav>
-        </header>
+        <div className="App">
+            {/*creating the navbar*/}
+            <header>
+                <img className="logo" src="Logo.jpg" alt="logo"/>
+                <nav>
+                    <ul className="navbar">
+                        <li><a href="/Page.html">About Us</a> </li>
+                        <li><a href="#">Career</a> </li>
+                        <li><a href="#">Departments</a> </li>
+                        <li><Link to="/sign.jsx">Sign in</Link></li>
+                    </ul>
+                </nav>
+            </header>
+            <div>
+                <h1>First React App</h1>
+            </div>
+        </div>
     </React.Fragment>
+    );
 }
+
+export default Main;
