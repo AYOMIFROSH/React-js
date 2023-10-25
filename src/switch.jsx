a8e51c70707d9fe221309839a060d596db135ff9
tayo <rhymeanabel@gmail.com>
2023-10-25 10:53:12 +0100

    updating src/switch.jsx

--- src/switch.jsx
+++ src/switch.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Sign from "./sign";
-import { Link } from 'react-router-dom';
+import Myimage from './Logo.jpg'
+
 
 const Main = () => {
     return (
     <React.Fragment>
         <div className="App">
-            {/*creating the navbar*/}
             <header>
-                <img className="logo" src="Logo.jpg" alt="logo"/>
-                <nav>
-                    <ul className="navbar">
-                        <li><a href="/Page.html">About Us</a> </li>
-                        <li><a href="#">Career</a> </li>
-                        <li><a href="#">Departments</a> </li>
-                        <li><Link to="/sign.jsx">Sign in</Link></li>
-                    </ul>
-                </nav>
+                <img className="logo" src= {Myimage} alt="logo"/>
+                    <nav>
+                        <ul className="navbar">
+                            <li><a href="/Page.html">About Us</a> </li>
+                            <li><a href="#">Career</a> </li>
+                            <li><a href="#">Departments</a> </li>
+                            <li><a href="./sign">sign in</a></li>
+                        </ul>
+                    </nav>
             </header>
             <div>
                 <h1>First React App</h1>
